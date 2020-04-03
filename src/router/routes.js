83f51612dddcd89f983fc2465d44ab074eb66be5
Tumen Bairov <bairovt@gmail.com>
2020-04-03 16:52:16 +0900

    initial

---
+++ src/router/routes.js
@@ -0,0 +1,22 @@
+const routes = [
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/Index.vue") }]
+  },
+  {
+    path: "/calc",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/Calc.vue") }]
+  }
+];
+
+// Always leave this as last one
+if (process.env.MODE !== "ssr") {
+  routes.push({
+    path: "*",
+    component: () => import("pages/Error404.vue")
+  });
+}
+
+export default routes;
