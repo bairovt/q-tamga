fc0596f57acdd81dff044743941965095471e67f
Tumen Bairov <bairovt@gmail.com>
2020-05-18 14:35:46 +0900

    nomenclature search

--- src/pages/nomenclature/Nomens.vue
+++ src/pages/nomenclature/Nomens.vue
@@ -3,13 +3,25 @@
     <q-breadcrumbs>
       <q-breadcrumbs-el label="Номенклатура" />
     </q-breadcrumbs>
+    <div class="row q-mb-lg">
+      <div class="col">
+        <q-input
+          v-model="search"
+          placeholder="поиск"
+          outlined
+          @input="doSearch()"
+          clearable
+          debounce="500"
+        ></q-input>
+      </div>
+    </div>
 
     <q-list>
       <div v-for="nomen in nomens" :key="nomen._key">
         <q-item :to="`/nomens/${nomen._key}`" class="q-pa-xs">
           <q-item-section>
             <q-item-label caption lines="1"
-              >тнвэд {{ nomen.tnved }}</q-item-label
+              >тнвэд {{ nomen.tnved }} ({{ nomen.measure }})</q-item-label
             >
             <q-item-label lines="3">{{ nomen.name }}</q-item-label>
           </q-item-section>
@@ -28,17 +40,22 @@ export default {
   name: 'PageNomens',
   data() {
     return {
+      search: '',
       nomens: []
     };
   },
   methods: {
     getNomens() {
+      const search = this.search ? this.search : '';
       this.$axios
-        .get('/api/nomenclature')
+        .get(`/api/nomenclature?search=${search}`)
         .then(resp => {
           this.nomens = resp.data.nomens;
         })
         .catch(console.error);
+    },
+    doSearch() {
+      this.getNomens();
     }
   },
   created() {
